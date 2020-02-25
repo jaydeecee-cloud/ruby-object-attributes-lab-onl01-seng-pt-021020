de328e870684f8d80619ddd4f5168597f1c0bba5
Joseph Choi <jdc07@live.com>
2020-02-25 18:31:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,1 @@
-class Dog
-    def name
-    @name
-  end
 
-  def name=(dog_name)
-    @name = dog_name
-  end
-end
-
-class Dog
-    def breed
-    @breed
-  end
-
-    def breed =(breed)
-    @breed = breed
-  end
-end
